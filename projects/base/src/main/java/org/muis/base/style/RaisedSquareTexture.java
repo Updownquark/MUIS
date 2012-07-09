ecda82251ec12a3bbe2194371678a8083f38f558
Andrew <abutler@psl.nmsu.edu>
2012-07-09 16:22:52 -0600

    #6: Implemented the raised-square texture

---
+++ projects/base/src/main/java/org/muis/base/style/RaisedSquareTexture.java
@@ -0,0 +1,103 @@
+package org.muis.base.style;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+
+import org.muis.core.MuisElement;
+import org.muis.core.style.Texture;
+
+/** Renders a raised, square, button-looking texture over an element */
+public class RaisedSquareTexture implements Texture
+{
+	@Override
+	public void render(Graphics2D graphics, MuisElement element, Rectangle area)
+	{
+		int w = element.getWidth();
+		int h = element.getHeight();
+		org.muis.core.style.Size radius = element.getStyle().get(org.muis.core.style.BackgroundStyles.cornerRadius);
+		int wRad = radius.evaluate(w);
+		int hRad = radius.evaluate(h);
+		graphics.setColor(org.muis.core.MuisUtils.getBackground(element.getStyle()));
+		graphics.fillRect(0, 0, w, h);
+		float source = element.getStyle().get(org.muis.core.style.LightedStyle.lightSource).floatValue();
+		float maxShading = element.getStyle().get(org.muis.core.style.LightedStyle.maxShadingAmount).floatValue();
+		Color light = element.getStyle().get(org.muis.core.style.LightedStyle.lightColor);
+		Color shadow = element.getStyle().get(org.muis.core.style.LightedStyle.shadowColor);
+		int lightRGB = light.getRGB() & 0xffffff;
+		int shadowRGB = shadow.getRGB() & 0xffffff;
+		int sin = -(int) Math.round(255 * Math.sin(source * Math.PI / 180));
+		int cos = (int) Math.round(255 * Math.sin(source * Math.PI / 180));
+
+		// Top and bottom sides
+		for(int y = 0; y < hRad; y++)
+		{
+			int startX = wRad * y / hRad + 1;
+			int endX = w - startX - 1;
+			int shading = (int) (maxShading * sin * (hRad - y) / hRad);
+			Color lineColor;
+			if(shading > 0)
+				lineColor = new Color(lightRGB | (shading << 24), true);
+			else if(shading < 0)
+				lineColor = new Color(shadowRGB | ((-shading) << 24), true);
+			else
+				lineColor = null;
+
+			if(lineColor != null)
+			{
+				graphics.setColor(lineColor);
+				graphics.drawLine(startX, y, endX, y);
+			}
+
+			shading = (int) (maxShading * -sin * (hRad - y) / hRad);
+			if(shading > 0)
+				lineColor = new Color(lightRGB | (shading << 24), true);
+			else if(shading < 0)
+				lineColor = new Color(shadowRGB | ((-shading) << 24), true);
+			else
+				lineColor = null;
+
+			if(lineColor != null)
+			{
+				graphics.setColor(lineColor);
+				graphics.drawLine(startX, h - y - 1, endX, h - y - 1);
+			}
+		}
+
+		// Left and right sides
+		for(int x = 0; x < wRad; x++)
+		{
+			int startY = hRad * x / wRad + 1;
+			int endY = h - startY - 1;
+			int shading = (int) (maxShading * -cos * (wRad - x) / wRad);
+			Color lineColor;
+			if(shading > 0)
+				lineColor = new Color(lightRGB | (shading << 24), true);
+			else if(shading < 0)
+				lineColor = new Color(shadowRGB | ((-shading) << 24), true);
+			else
+				lineColor = null;
+
+			if(lineColor != null)
+			{
+				graphics.setColor(lineColor);
+				graphics.drawLine(x, startY, x, endY);
+			}
+
+			shading = (int) (maxShading * cos * (wRad - x) / wRad);
+			if(shading > 0)
+				lineColor = new Color(lightRGB | (shading << 24), true);
+			else if(shading < 0)
+				lineColor = new Color(shadowRGB | ((-shading) << 24), true);
+			else
+				lineColor = null;
+
+			if(lineColor != null)
+			{
+				graphics.setColor(lineColor);
+				graphics.drawLine(w - x - 1, startY, w - x - 1, endY);
+			}
+		}
+		// TODO Auto-generated method stub
+	}
+}

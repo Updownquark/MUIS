0f289ee25d14cc69f5666d5d81841021190216d5
Andrew <abutler@psl.nmsu.edu>
2012-10-26 16:06:57 -0600

    #32: Added the MuisContainer interface.  Also added a couple annotations that I think I'll use for templates.  Not sure how everything will fit together yet, so these may be trash later.

---
+++ projects/MUIS/src/main/java/org/muis/core/tags/Template.java
@@ -0,0 +1,10 @@
+package org.muis.core.tags;
+
+/** Tells {@link org.muis.core.MuisTemplate MuisTemplate} that the element this is tagged on needs to load a template file */
+public @interface Template {
+	/** The location of the file to load the templated widget's definition from */
+	String location();
+
+	/** All attach points for this widget */
+	AttachPoint [] attachPoints();
+}

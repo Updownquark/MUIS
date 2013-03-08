2896f76307e6ff9cf07347d43cd0ffbcd464fc9c
Andrew <abutler@psl.nmsu.edu>
2013-03-08 13:40:18 -0700

    Converted Button to a template widget and updated ButtonTest.

--- projects/base/src/main/java/org/muis/base/widget/Button.java
+++ projects/base/src/main/java/org/muis/base/widget/Button.java
@@ -1,13 +1,18 @@
 package org.muis.base.widget;
 
-import org.muis.core.MuisLayout;
+import org.muis.core.tags.Template;
 
 /** Implements a button. Buttons can be set to toggle mode or normal mode. Buttons are containers that may have any type of content in them. */
-public class Button extends org.muis.core.LayoutContainer
-{
+@Template(location = "../../../../button.muis")
+public class Button extends org.muis.core.MuisTemplate2 {
 	@Override
-	protected MuisLayout getDefaultLayout()
-	{
-		return new org.muis.base.layout.SimpleLayout();
+	public void doLayout() {
+		org.muis.core.MuisElement contents = getElement(getTemplate().getAttachPoint("contents"));
+		org.muis.core.style.Size radius = getStyle().getSelf().get(org.muis.core.style.BackgroundStyles.cornerRadius);
+		int w = getWidth();
+		int h = getHeight();
+		int lOff = radius.evaluate(w);
+		int tOff = radius.evaluate(h);
+		contents.setBounds(lOff, tOff, w - lOff - lOff, h - tOff - tOff);
 	}
 }

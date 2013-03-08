0432f4deff7a60da7895eb4bee58c7bfdb5ed4e4
Andrew <abutler@psl.nmsu.edu>
2013-03-08 14:00:15 -0700

    #32: Renamed MuisTemplate2 to MuisTemplate

--- projects/MUIS/src/main/java/org/muis/core/tags/Template.java
+++ projects/MUIS/src/main/java/org/muis/core/tags/Template.java
@@ -3,7 +3,7 @@ package org.muis.core.tags;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 
-/** Tells {@link org.muis.core.MuisTemplate2 MuisTemplate} that the element this is tagged on needs to load a template file */
+/** Tells {@link org.muis.core.MuisTemplate MuisTemplate} that the element this is tagged on needs to load a template file */
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Template {
 	/** The location of the file to load the templated widget's definition from */
--- projects/base/src/main/java/org/muis/base/widget/Button.java
+++ projects/base/src/main/java/org/muis/base/widget/Button.java
@@ -4,7 +4,7 @@ import org.muis.core.tags.Template;
 
 /** Implements a button. Buttons can be set to toggle mode or normal mode. Buttons are containers that may have any type of content in them. */
 @Template(location = "../../../../button.muis")
-public class Button extends org.muis.core.MuisTemplate2 {
+public class Button extends org.muis.core.MuisTemplate {
 	@Override
 	public void doLayout() {
 		org.muis.core.MuisElement contents = getElement(getTemplate().getAttachPoint("contents"));

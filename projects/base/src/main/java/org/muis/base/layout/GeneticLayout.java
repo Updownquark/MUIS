a2e84dd7073573be9172117fc35daf026b150129
Andrew <Updownquark@gmail.com>
2013-03-20 22:33:09 -0600

    #7: Implemented more of GeneticSolver.  It might work now.  Only single-threaded atm. Implemented more of GeneticLayout using the solver.  Had to modify LayoutConstraint, so I'll need to update all those implementations. After that, gotta write more generic constraints, then the flow-specific constraints, and try it out.

--- projects/base/src/main/java/org/muis/base/layout/GeneticLayout.java
+++ projects/base/src/main/java/org/muis/base/layout/GeneticLayout.java
@@ -1,7 +1,13 @@
 package org.muis.base.layout;
 
+import java.awt.Dimension;
 import java.awt.Rectangle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
 
+import org.muis.base.util.GeneticSolver;
+import org.muis.base.util.GeneticSolver.Genome;
 import org.muis.core.MuisElement;
 
 public class GeneticLayout {
@@ -12,17 +18,116 @@ public class GeneticLayout {
 	}
 
 	public static interface LayoutConstraint {
-		boolean isViolated(MuisElement container, LayoutScratchPad layout);
+		boolean isViolated(MuisElement container, Dimension size, LayoutScratchPad layout);
 
-		float getViolation(MuisElement container, LayoutScratchPad layout);
+		float getViolation(MuisElement container, Dimension size, LayoutScratchPad layout);
 	}
 
+	private static class LayoutConstraintHolder {
+		final LayoutConstraint constraint;
+
+		final boolean variableCost;
+
+		final float cost;
+
+		LayoutConstraintHolder(LayoutConstraint cnstrnt, boolean vc, float cst) {
+			constraint = cnstrnt;
+			variableCost = vc;
+			cost = cst;
+		}
+	}
+
+	private ArrayList<LayoutConstraintHolder> theConstraints;
+
 	public GeneticLayout() {
+		theConstraints = new ArrayList<>();
 	}
 
 	public void addConstraint(LayoutConstraint constraint, boolean variableCost, float cost) {
+		theConstraints.add(new LayoutConstraintHolder(constraint, variableCost, cost));
 	}
 
 	public void layout(int width, int height, MuisElement container, MuisElement... children) {
+	}
+
+	private class GeneticLayoutMD {
+		MuisElement container;
+
+		Dimension size;
+
+		MuisElement [] elements;
+
+		GeneticLayoutMD(MuisElement cont, Dimension sz, MuisElement [] els) {
+			container = cont;
+			size = sz;
+			elements = els;
+		}
+	}
+
+	private class ScratchPadImpl implements LayoutScratchPad {
+		private MuisElement [] theElements;
+
+		private HashMap<MuisElement, Rectangle> theBounds;
+
+		ScratchPadImpl(MuisElement [] els) {
+			theElements = els;
+			theBounds = new HashMap<>();
+			for(MuisElement el : theElements) {
+				theBounds.put(el, new Rectangle(el.bounds().getX(), el.bounds().getY(), el.bounds().getWidth(), el.bounds().getHeight()));
+			}
+		}
+
+		@Override
+		public Iterator<MuisElement> iterator() {
+			return prisms.util.ArrayUtils.iterator(theElements, true);
+		}
+
+		@Override
+		public int size() {
+			return theElements.length;
+		}
+
+		@Override
+		public Rectangle get(MuisElement element) {
+			return theBounds.get(element);
+		}
+	}
+
+	private class LayoutTester implements GeneticSolver.GenomeTester {
+		private MuisElement theContainer;
+
+		private Dimension theSize;
+
+		private ScratchPadImpl theScratchPad;
+
+		LayoutTester(MuisElement container, Dimension size, MuisElement [] elements) {
+			theContainer = container;
+			theSize = size;
+			theScratchPad = new ScratchPadImpl(elements);
+		}
+
+		@Override
+		public float getFitness(Genome genome) {
+			float ret = 0;
+			for(LayoutConstraintHolder holder : theConstraints) {
+				if(holder.variableCost) {
+					ret += holder.constraint.getViolation(theContainer, theSize, theScratchPad) * holder.cost;
+				} else if(holder.constraint.isViolated(theContainer, theSize, theScratchPad)) {
+					ret += holder.cost;
+				}
+			}
+			return ret;
+		}
+
+		@Override
+		public void close() {
+		}
+	}
+
+	private class LayoutTesterMaker implements GeneticSolver.GenomeTesterMaker<GeneticLayoutMD> {
+		@Override
+		public LayoutTester createTester(GeneticLayoutMD metadata) {
+			return new LayoutTester(metadata.container, metadata.size, metadata.elements);
+		}
 	}
 }
--- projects/base/src/main/java/org/muis/base/util/GeneticSolver.java
+++ projects/base/src/main/java/org/muis/base/util/GeneticSolver.java
@@ -1,8 +1,9 @@
 package org.muis.base.util;
 
 import java.util.Iterator;
+import java.util.Random;
 
-public class GeneticSolver {
+public class GeneticSolver<T> {
 	public static class Genome {
 		private float [] theGenes;
 
@@ -14,22 +15,44 @@ public class GeneticSolver {
 			return theGenes;
 		}
 
-		Genome mate(Genome mate, float mutationFactor) {
-
+		void mate(Genome mate, float mutationFactor, Random random, Genome child) {
+			for(int i = 0; i < theGenes.length; i++) {
+				float rand = getMutation(random.nextFloat(), mutationFactor);
+				child.theGenes[i] = theGenes[i] + (mate.theGenes[i] - theGenes[i]) * rand;
+				if((rand * 1000) % 13 <= 2)
+					child.theGenes[i] += getMutation(random.nextFloat(), mutationFactor);
+			}
 		}
 
-		Genome mutate(float mutationFactor) {
+		void mutate(float mutationFactor, Random random, Genome spawn) {
+			for(int i = 0; i < theGenes.length; i++) {
+				float rand = getMutation(random.nextFloat(), mutationFactor);
+				spawn.theGenes[i] = theGenes[i] * rand;
+				if((rand * 1000) % 13 <= 2)
+					spawn.theGenes[i] += getMutation(random.nextFloat(), mutationFactor);
+			}
 		}
 	}
 
-	public static interface GenomeTester {
+	public static interface GenomeTester extends AutoCloseable {
 		float getFitness(Genome genome);
+
+		@Override
+		void close();
+	}
+
+	public static interface GenomeTesterMaker<T> {
+		GenomeTester createTester(T metadata);
 	}
 
 	public static class GeneticSolution implements Comparable<GeneticSolution> {
-		public final Genome genome;
+		Genome genome;
 
-		public final float fitness;
+		float fitness;
+
+		GeneticSolution(int size) {
+			genome = new Genome(size);
+		}
 
 		public GeneticSolution(Genome genes, float fit) {
 			genome = genes;
@@ -45,6 +68,14 @@ public class GeneticSolver {
 				return 1;
 			else
 				return 0;
+		}
+
+		public Genome getGenome() {
+			return genome;
+		}
+
+		public float getFitness() {
+			return fitness;
 		}
 	}
 
@@ -68,11 +99,37 @@ public class GeneticSolver {
 		}
 	}
 
+	private static class SolutionPool {
+		private java.util.concurrent.ConcurrentLinkedQueue<GeneticSolution> theSolutionSet;
+
+		private int theGenomeSize;
+
+		SolutionPool(int genomeSize) {
+			theSolutionSet = new java.util.concurrent.ConcurrentLinkedQueue<>();
+			theGenomeSize = genomeSize;
+		}
+
+		GeneticSolution get() {
+			GeneticSolution ret = theSolutionSet.poll();
+			if(ret == null)
+				ret = new GeneticSolution(theGenomeSize);
+			return ret;
+		}
+
+		void retire(GeneticSolution solution) {
+			theSolutionSet.add(solution);
+		}
+	}
+
 	private static final int TRIES = 5;
+
+	private static final float MUTATION_RATE = 1.3f;
 
 	private static final int MATE_PREFERENCE_FACTOR = 4;
 
-	private final GenomeTester theTester;
+	private final GenomeTesterMaker<T> theTesterMaker;
+
+	private Random theRandom;
 
 	private int theMinSolutions;
 
@@ -82,12 +139,17 @@ public class GeneticSolver {
 
 	private int thePopulationCap;
 
-	public GeneticSolver(GenomeTester tester) {
-		theTester = tester;
+	public GeneticSolver(GenomeTesterMaker<T> testerMaker) {
+		theTesterMaker = testerMaker;
+		theRandom = new Random();
 		theMinSolutions = 1;
 		theMinFitness = 100;
 		theMaxGenerations = 10;
 		thePopulationCap = 100;
+	}
+
+	public void setRandom(Random random) {
+		theRandom = random;
 	}
 
 	public void setMinSolutions(int minSolutions) {
@@ -106,15 +168,24 @@ public class GeneticSolver {
 		thePopulationCap = popCap;
 	}
 
-	public GeneticResults solve(Genome... init) {
+	public GeneticResults solve(T metaData, Genome... init) {
+		if(init.length == 0)
+			throw new IllegalArgumentException("Genetic solver requires one or more initialization genomes");
+		int genomeSize = init[0].getGenes().length;
+		for(int i = 1; i < init.length; i++) {
+			if(init[i].getGenes().length != genomeSize)
+				throw new IllegalArgumentException("All initialization genomes must be of the same size");
+		}
+		SolutionPool pool = new SolutionPool(init[0].getGenes().length);
+		GenomeTester tester = theTesterMaker.createTester(metaData);
 		GeneticSolution [] population = new GeneticSolution[init.length];
 		for(int i = 0; i < init.length; i++)
-			population[i] = new GeneticSolution(init[i], theTester.getFitness(init[i]));
+			population[i] = new GeneticSolution(init[i], tester.getFitness(init[i]));
 		java.util.Arrays.sort(population);
 		int generations;
 		java.util.TreeSet<GeneticSolution> solutionSet = new java.util.TreeSet<>();
 		for(generations = 0; generations < theMaxGenerations && population.length > 0; generations++) {
-			population = evolve(population, solutionSet);
+			population = evolve(population, solutionSet, tester, pool);
 			int good = 0;
 			for(GeneticSolution soln : population) {
 				if(soln.fitness >= theMinFitness)
@@ -142,23 +213,31 @@ public class GeneticSolver {
 		return new GeneticResults(solutions, generations);
 	}
 
-	private GeneticSolution [] evolve(GeneticSolution [] pop, java.util.TreeSet<GeneticSolution> solutionSet) {
+	private float nr() {
+		return theRandom.nextFloat();
+	}
+
+	private GeneticSolution [] evolve(GeneticSolution [] pop, java.util.TreeSet<GeneticSolution> solutionSet, GenomeTester tester,
+		SolutionPool pool) {
 		for(int p = 0; p < pop.length; p++) {
 			solutionSet.add(pop[p]);
 			while(solutionSet.size() > thePopulationCap)
-				solutionSet.pollLast();
+				pool.retire(solutionSet.pollLast());
 
 			// Mutate each genome a certain number of times--fit genomes get mutated more times
 			int numMutations = (int) Math.ceil(thePopulationCap * 2f / pop.length / p);
 			for(int mutation = 0; mutation < numMutations; mutation++) {
 				for(int tries = 0; tries < TRIES; tries++) {
-					Genome child = pop[p].genome.mutate(mutation * 1f / numMutations);
-					float fitness = theTester.getFitness(child);
-					if(fitness != Float.NEGATIVE_INFINITY) {
-						solutionSet.add(new GeneticSolution(child, fitness));
+					GeneticSolution child = pool.get();
+					pop[p].genome.mutate((mutation + 1f) / numMutations, theRandom, child.genome);
+					child.fitness = tester.getFitness(child.genome);
+					if(child.fitness != Float.NEGATIVE_INFINITY) {
+						solutionSet.add(child);
 						while(solutionSet.size() > thePopulationCap)
-							solutionSet.pollLast();
+							pool.retire(solutionSet.pollLast());
 						break;
+					} else {
+						pool.retire(child);
 					}
 				}
 			}
@@ -170,20 +249,23 @@ public class GeneticSolver {
 					// Take the minimum of a few random indexes so simulate preferential mate selection
 					int mateIdx = pop.length - 1;
 					for(int mateTryIdx = 0; mateTryIdx < MATE_PREFERENCE_FACTOR; mateTryIdx++) {
-						int mateTry = (int) Math.round(Math.random() * (pop.length - 1));
+						int mateTry = Math.round(nr() * (pop.length - 1));
 						if(mateTry < mateIdx)
 							mateIdx = mateTry;
 					}
 					// Remove the possibility of something mating with itself
 					if(mateIdx >= p)
 						mateIdx++;
-					Genome child = pop[p].genome.mate(pop[mateIdx].genome, mateNum * 1f / numMates);
-					float fitness = theTester.getFitness(child);
-					if(fitness != Float.NEGATIVE_INFINITY) {
-						solutionSet.add(new GeneticSolution(child, fitness));
+					GeneticSolution child = pool.get();
+					pop[p].genome.mate(pop[mateIdx].genome, mateNum * 1f / numMates, theRandom, child.genome);
+					child.fitness = tester.getFitness(child.genome);
+					if(child.fitness != Float.NEGATIVE_INFINITY) {
+						solutionSet.add(child);
 						while(solutionSet.size() > thePopulationCap)
-							solutionSet.pollLast();
+							pool.retire(solutionSet.pollLast());
 						break;
+					} else {
+						pool.retire(child);
 					}
 				}
 			}
@@ -193,5 +275,9 @@ public class GeneticSolver {
 			return solutionSet.toArray(pop); // Reuse if we can
 		else
 			return solutionSet.toArray(new GeneticSolution[solutionSet.size()]);
+	}
+
+	static float getMutation(float random, float mutationFactor) {
+		return ((random - .5f) * MUTATION_RATE + MUTATION_RATE / 2f) * mutationFactor;
 	}
 }

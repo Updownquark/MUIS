f3237e29abae795b9de4e0c2cd33469dbc3a125c
Andrew <Updownquark@gmail.com>
2013-06-01 11:58:25 -0600

    #6: Modified the model to change the background of the whole MUIS body when the button is clicked, just to show that it can do stuff.

--- projects/tests/src/main/java/org/muis/test/model/ButtonTestModel.java
+++ projects/tests/src/main/java/org/muis/test/model/ButtonTestModel.java
@@ -4,12 +4,30 @@ import org.muis.core.MuisElement;
 
 /** A simple MUIS model for the button test */
 public class ButtonTestModel {
+	private int theCount;
 	/**
 	 * Called when the user clicks the big button
-	 * 
+	 *
 	 * @param element The button
 	 */
 	public void clicked(MuisElement element) {
-		System.out.println("Clicked!");
+		if(element == null)
+			return;
+		org.muis.core.BodyElement body = element.getDocument().getRoot();
+		java.awt.Color color = getColor();
+		body.getStyle().getSelf().set(org.muis.core.style.BackgroundStyles.color, color);
+		theCount++;
+	}
+
+	private java.awt.Color getColor() {
+		switch (theCount % 3) {
+		case 0:
+			return org.muis.core.style.Colors.red;
+		case 1:
+			return org.muis.core.style.Colors.blue;
+		case 2:
+			return org.muis.core.style.Colors.green;
+		}
+		return org.muis.core.style.Colors.black;
 	}
 }

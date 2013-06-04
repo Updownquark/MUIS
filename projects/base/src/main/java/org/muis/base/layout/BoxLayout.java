661297e817af9806e700c7e01c83f98b7b5c965f
Andrew <Updownquark@gmail.com>
2013-06-03 22:18:25 -0600

    #1: Fixed and completed BoxLayout

--- projects/base/src/main/java/org/muis/base/layout/BoxLayout.java
+++ projects/base/src/main/java/org/muis/base/layout/BoxLayout.java
@@ -2,6 +2,8 @@ package org.muis.base.layout;
 
 import static org.muis.core.layout.LayoutAttributes.*;
 
+import java.awt.Rectangle;
+
 import org.muis.core.MuisElement;
 import org.muis.core.MuisLayout;
 import org.muis.core.layout.*;
@@ -21,12 +23,13 @@ public class BoxLayout implements MuisLayout {
 	/** Creates a box layout */
 	public BoxLayout() {
 		theListener = CompoundListener.create(this);
-		theListener.acceptAll(direction, alignment).onChange(CompoundListener.layout);
+		theListener.acceptAll(direction, alignment, crossAlignment).onChange(CompoundListener.layout);
 		theListener.child().acceptAll(width, minWidth, maxWidth, height, minHeight, maxHeight).onChange(CompoundListener.layout);
 	}
 
 	@Override
 	public void initChildren(MuisElement parent, MuisElement [] children) {
+		theListener.listenerFor(parent);
 	}
 
 	@Override
@@ -39,11 +42,12 @@ public class BoxLayout implements MuisLayout {
 
 	@Override
 	public void remove(MuisElement parent) {
+		theListener.dropFor(parent);
 	}
 
 	@Override
 	public SizeGuide getWSizer(MuisElement parent, MuisElement [] children) {
-		Direction dir = parent.atts().get(LayoutAttributes.direction, Direction.RIGHT);
+		Direction dir = parent.atts().get(direction, Direction.RIGHT);
 		Size margin = parent.getStyle().getSelf().get(LayoutStyles.margin);
 		Size padding = parent.getStyle().getSelf().get(LayoutStyles.padding);
 		switch (dir) {
@@ -59,7 +63,7 @@ public class BoxLayout implements MuisLayout {
 
 	@Override
 	public SizeGuide getHSizer(MuisElement parent, MuisElement [] children) {
-		Direction dir = parent.atts().get(LayoutAttributes.direction, Direction.RIGHT);
+		Direction dir = parent.atts().get(direction, Direction.RIGHT);
 		Size margin = parent.getStyle().getSelf().get(LayoutStyles.margin);
 		Size padding = parent.getStyle().getSelf().get(LayoutStyles.padding);
 		switch (dir) {
@@ -174,35 +178,132 @@ public class BoxLayout implements MuisLayout {
 
 	@Override
 	public void layout(MuisElement parent, final MuisElement [] children) {
-		java.awt.Rectangle bounds = new java.awt.Rectangle();
-		final Direction dir = parent.atts().get(LayoutAttributes.direction, Direction.RIGHT);
-		Alignment align = parent.atts().get(LayoutAttributes.alignment, Alignment.begin);
+		final Direction dir = parent.atts().get(direction, Direction.RIGHT);
+		Alignment align = parent.atts().get(alignment, Alignment.begin);
+		Alignment crossAlign = parent.atts().get(crossAlignment, Alignment.begin);
+		final Size margin = parent.getStyle().getSelf().get(LayoutStyles.margin);
+		final Size padding = parent.getStyle().getSelf().get(LayoutStyles.padding);
 
 		final int parallelSize = parent.bounds().get(dir.getOrientation()).getSize();
 		final int crossSize = parent.bounds().get(dir.getOrientation().opposite()).getSize();
-		LayoutUtils.LayoutInterpolation<int []> result = LayoutUtils.interpolate(new LayoutUtils.LayoutChecker<int []>() {
+		final int crossSizeWOMargin = crossSize - margin.evaluate(crossSize) * 2;
+		LayoutUtils.LayoutInterpolation<LayoutSize []> result = LayoutUtils.interpolate(new LayoutUtils.LayoutChecker<LayoutSize []>() {
 			@Override
-			public int [] getLayoutValue(LayoutGuideType type) {
-				int [] ret = new int[children.length];
-				for(int i = 0; i < ret.length; i++)
-					ret[i] = LayoutUtils.getSize(children[i], dir.getOrientation(), type, parallelSize, crossSize, false, null);
+			public LayoutSize [] getLayoutValue(LayoutGuideType type) {
+				LayoutSize [] ret = new LayoutSize[children.length];
+				for(int i = 0; i < ret.length; i++) {
+					ret[i] = new LayoutSize();
+					LayoutUtils.getSize(children[i], dir.getOrientation(), type, parallelSize, crossSizeWOMargin, true, ret[i]);
+				}
 				return ret;
 			}
 
 			@Override
-			public int getSize(int [] layoutValue) {
-				int ret = 0;
-				for(int lv : layoutValue)
-					ret += lv;
-				return ret;
+			public int getSize(LayoutSize [] layoutValue) {
+				LayoutSize ret = new LayoutSize();
+				ret.add(margin);
+				ret.add(margin);
+				for(int i = 0; i < layoutValue.length; i++) {
+					if(i > 0)
+						ret.add(padding);
+					ret.add(layoutValue[i]);
+				}
+				return ret.getTotal(parallelSize);
 			}
 		}, parallelSize, true, align == Alignment.justify);
 
+		Rectangle [] bounds = new Rectangle[children.length];
 		for(int c = 0; c < children.length; c++) {
-			int mainSize = result.lowerValue[c];
+			bounds[c] = new Rectangle();
+			int mainSize = result.lowerValue[c].getTotal(parallelSize);
 			if(result.proportion > 0)
-				mainSize += Math.round(result.proportion * (result.upperValue[c] - result.lowerValue[c]));
-			// TODO
+				mainSize += Math.round(result.proportion
+					* (result.upperValue[c].getTotal(parallelSize) - result.lowerValue[c].getTotal(parallelSize)));
+			LayoutUtils.setSize(bounds[c], dir.getOrientation(), mainSize);
+
+			int prefCrossSize = LayoutUtils.getSize(children[c], dir.getOrientation().opposite(), LayoutGuideType.pref, crossSize,
+				mainSize, false, null);
+			int oppSize;
+			if(crossSize < prefCrossSize) {
+				int minCrossSize = LayoutUtils.getSize(children[c], dir.getOrientation().opposite(), LayoutGuideType.min, crossSize,
+					mainSize, false, null);
+				if(crossSize < minCrossSize)
+					oppSize = minCrossSize;
+				else
+					oppSize = crossSize;
+			} else if(align == Alignment.justify) {
+				int maxCrossSize = LayoutUtils.getSize(children[c], dir.getOrientation().opposite(), LayoutGuideType.max, crossSize,
+					mainSize, false, null);
+				if(crossSize < maxCrossSize)
+					oppSize = crossSize;
+				else
+					oppSize = maxCrossSize;
+			} else
+				oppSize = prefCrossSize;
+			LayoutUtils.setSize(bounds[c], dir.getOrientation().opposite(), oppSize);
 		}
+
+		// Main alignment
+		switch (align) {
+		case begin:
+		case end:
+			int pos = 0;
+			for(int c = 0; c < children.length; c++) {
+				int childPos = pos;
+				if(c == 0)
+					childPos += margin.evaluate(parallelSize);
+				else
+					childPos += padding.evaluate(parallelSize);
+				LayoutUtils.setPos(bounds[c], dir.getOrientation(), align == Alignment.begin ? childPos : parallelSize - childPos
+					- LayoutUtils.getSize(bounds[c], dir.getOrientation()));
+				pos = childPos + LayoutUtils.getSize(bounds[c], dir.getOrientation());
+			}
+			break;
+		case center:
+		case justify:
+			int freeSpace = parallelSize;
+			freeSpace -= margin.evaluate(parallelSize);
+			for(int c = 0; c < bounds.length; c++) {
+				if(c > 0)
+					freeSpace -= padding.evaluate(parallelSize);
+				freeSpace -= LayoutUtils.getSize(bounds[c], dir.getOrientation());
+			}
+
+			pos = 0;
+			int usedSpace = 0;
+			for(int c = 0; c < children.length; c++) {
+				int extraSpace = (freeSpace - usedSpace) / (children.length + 1 - c);
+				int childPos = pos + extraSpace;
+				if(c == 0)
+					childPos += margin.evaluate(parallelSize);
+				else
+					childPos += padding.evaluate(parallelSize);
+				LayoutUtils.setPos(bounds[c], dir.getOrientation(), align == Alignment.begin ? childPos : parallelSize - childPos
+					- LayoutUtils.getSize(bounds[c], dir.getOrientation()));
+				pos = childPos + LayoutUtils.getSize(bounds[c], dir.getOrientation());
+				usedSpace += extraSpace;
+			}
+		}
+
+		// Cross alignment
+		switch (crossAlign) {
+		case begin:
+			for(Rectangle bound : bounds)
+				LayoutUtils.setPos(bound, dir.getOrientation().opposite(), margin.evaluate(crossSize));
+			break;
+		case end:
+			for(Rectangle bound : bounds)
+				LayoutUtils.setPos(bound, dir.getOrientation().opposite(),
+					crossSize - LayoutUtils.getSize(bound, dir.getOrientation().opposite()) - margin.evaluate(crossSize));
+			break;
+		case center:
+		case justify:
+			for(Rectangle bound : bounds)
+				LayoutUtils.setPos(bound, dir.getOrientation().opposite(),
+					margin.evaluate(crossSize) + (crossSize - LayoutUtils.getSize(bound, dir.getOrientation().opposite())) / 2);
+		}
+
+		for(int c = 0; c < children.length; c++)
+			children[c].bounds().setBounds(bounds[c].x, bounds[c].y, bounds[c].width, bounds[c].height);
 	}
 }

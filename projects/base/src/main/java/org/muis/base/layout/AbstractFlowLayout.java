f0597ce746ca2fc31940230e97d0e22a51ae712a
Andrew <Updownquark@gmail.com>
2013-06-08 16:02:36 -0600

    Got rid of some warnings in unused class AbstractFlowLayout

--- projects/base/src/main/java/org/muis/base/layout/AbstractFlowLayout.java
+++ projects/base/src/main/java/org/muis/base/layout/AbstractFlowLayout.java
@@ -5,15 +5,13 @@ import org.muis.core.MuisAttribute;
 /**
  * A flow layout is a layout that lays contents out one after another in one direction, perhaps using multiple rows, and obeys constraints
  * set on the contents.
- * 
+ *
  * Deleted the guts of this. Everything's been changed so much since I attempted this. Only thing that was worth saving was the notion of
  * the break policy (not currently used anywhere).
  */
-public abstract class AbstractFlowLayout
-{
+public abstract class AbstractFlowLayout {
 	/** Policies for creating multiple rows (or columns) of content */
-	public static enum BreakPolicy
-	{
+	public static enum BreakPolicy {
 		/** Never breaks a line (row or column) of content */
 		NEVER,
 		/** Breaks content as needed to fit in a container (the default) */
@@ -23,6 +21,6 @@ public abstract class AbstractFlowLayout
 	}
 
 	/** The attribute in the layout container that specifies the break policy for laying out items */
-	public static final MuisAttribute<BreakPolicy> FLOW_BREAK = new MuisAttribute<BreakPolicy>("flow-break",
-		new org.muis.core.MuisProperty.MuisEnumProperty<BreakPolicy>(BreakPolicy.class));
+	public static final MuisAttribute<BreakPolicy> FLOW_BREAK = new MuisAttribute<>("flow-break",
+		new org.muis.core.MuisProperty.MuisEnumProperty<>(BreakPolicy.class));
 }

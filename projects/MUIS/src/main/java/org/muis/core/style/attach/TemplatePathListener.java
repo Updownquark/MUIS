b079d36e6156828df5a44903ca69fe48569aa640
Andrew <abutler@psl.nmsu.edu>
2013-07-12 16:15:43 -0600

    #38: Tiny bit more work on the TemplatePathListener

--- projects/MUIS/src/main/java/org/muis/core/style/attach/TemplatePathListener.java
+++ projects/MUIS/src/main/java/org/muis/core/style/attach/TemplatePathListener.java
@@ -1,6 +1,10 @@
 package org.muis.core.style.attach;
 
-import org.muis.core.MuisElement;
+import org.muis.core.*;
+import org.muis.core.MuisConstants.Events;
+import org.muis.core.MuisTemplate.AttachPoint;
+import org.muis.core.event.MuisEvent;
+import org.muis.core.event.MuisEventListener;
 import org.muis.core.style.sheet.TemplatePath;
 
 public class TemplatePathListener {
@@ -24,6 +28,20 @@ public class TemplatePathListener {
 		if(theElement != null)
 			throw new IllegalStateException("This " + getClass().getSimpleName() + " is already listening to "
 				+ (element == theElement ? "this" : "a different") + " element.");
+		theElement.addListener(Events.ATTRIBUTE_SET, new MuisEventListener<MuisAttribute<?>>() {
+			@Override
+			public void eventOccurred(MuisEvent<MuisAttribute<?>> event, MuisElement evtElement) {
+				if(!(event.getValue().getType() instanceof MuisTemplate.TemplateStructure.RoleAttributeType))
+					return;
+				MuisAttribute<AttachPoint> roleAttr = (MuisAttribute<AttachPoint>) event.getValue();
+				roleChanged(roleAttr, theElement.atts().get(roleAttr));
+			}
+
+			@Override
+			public boolean isLocal() {
+				return true;
+			}
+		});
 	}
 
 	public void unlisten() {
@@ -40,5 +58,9 @@ public class TemplatePathListener {
 
 	public void removeListener(Listener listener) {
 		theListeners.remove(listener);
+	}
+
+	private void roleChanged(MuisAttribute<MuisTemplate.AttachPoint> roleAttr, MuisTemplate.AttachPoint role) {
+		int todo; // TODO
 	}
 }

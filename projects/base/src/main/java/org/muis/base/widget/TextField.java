1275d308fb816a36152d76f1972e7aaab5427ae4
Andrew <Updownquark@gmail.com>
2013-10-01 22:09:01 -0600

    #27: Added a link to its value's document model from TextField

--- projects/base/src/main/java/org/muis/base/widget/TextField.java
+++ projects/base/src/main/java/org/muis/base/widget/TextField.java
@@ -1,8 +1,19 @@
 package org.muis.base.widget;
 
+import org.muis.core.model.SimpleDocumentModel;
 import org.muis.core.tags.Template;
 
 /** A simple widget that takes and displays text input from the user */
 @Template(location = "../../../../text-field.muis")
 public class TextField extends org.muis.core.MuisTemplate {
+	private SimpleDocumentModel theDocument;
+
+	public TextField() {
+		life().runWhen(new Runnable() {
+			@Override
+			public void run() {
+				theDocument = ((org.muis.core.MuisTextElement) getElement(getTemplate().getAttachPoint("value"))).getDocumentModel();
+			}
+		}, org.muis.core.MuisConstants.CoreStage.INIT_CHILDREN.toString(), 1);
+	}
 }

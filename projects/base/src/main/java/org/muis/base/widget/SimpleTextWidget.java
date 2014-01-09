a279f3c4d168daf805f4a48c23bd23fb2824a5f5
Andrew <abutler@psl.nmsu.edu>
2014-01-09 15:40:28 -0700

    Javadoc

--- projects/base/src/main/java/org/muis/base/widget/SimpleTextWidget.java
+++ projects/base/src/main/java/org/muis/base/widget/SimpleTextWidget.java
@@ -1,5 +1,7 @@
 package org.muis.base.widget;
 
+/** A widget containing a {@link org.muis.core.model.SimpleDocumentModel} */
 public interface SimpleTextWidget {
+	/** @return This widget's document model */
 	org.muis.core.model.SimpleDocumentModel getDocumentModel();
 }

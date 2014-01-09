b496f92b20631eca770122881f37e86a0279a860
Andrew <abutler@psl.nmsu.edu>
2014-01-09 15:40:19 -0700

    #27: Fixed pasting and delete

--- projects/base/src/main/java/org/muis/base/model/SimpleTextEditing.java
+++ projects/base/src/main/java/org/muis/base/model/SimpleTextEditing.java
@@ -5,9 +5,11 @@ import org.muis.core.MuisElement;
 import org.muis.core.event.CharInputEvent;
 import org.muis.core.model.MuisBehavior;
 
+/** Behavior allowing keyboard input */
 public class SimpleTextEditing implements MuisBehavior<SimpleTextWidget> {
 	private org.muis.core.event.CharInputListener theInputListener;
 
+	/** Creates the behavior */
 	public SimpleTextEditing() {
 		theInputListener = new org.muis.core.event.CharInputListener(false) {
 			@Override
@@ -29,8 +31,12 @@ public class SimpleTextEditing implements MuisBehavior<SimpleTextWidget> {
 
 	}
 
+	/**
+	 * @param widget The widget that the character is being given to
+	 * @param ch The input character
+	 */
 	protected void charInput(SimpleTextWidget widget, char ch) {
-		if(ch < ' ' && ch != '\t' && ch != '\n' && ch != '\r')
+		if(ch < ' ' && ch != '\t' && ch != '\n' && ch != '\r' && ch != '\b' && ch != CharInputEvent.PASTE)
 			return;
 		org.muis.core.model.SimpleDocumentModel doc = widget.getDocumentModel();
 		boolean batchDeleted = false;
@@ -44,7 +50,26 @@ public class SimpleTextEditing implements MuisBehavior<SimpleTextWidget> {
 		} else if(ch == java.awt.event.KeyEvent.VK_DELETE) {
 			if(!batchDeleted && doc.getCursor() < doc.length() - 1)
 				doc.delete(doc.getCursor(), doc.getCursor() + 1);
+		} else if(ch == CharInputEvent.PASTE) {
+			pasteFromClipboard(widget);
 		} else
 			doc.insert(ch);
+	}
+
+	private static void pasteFromClipboard(SimpleTextWidget element) {
+		java.awt.datatransfer.Transferable contents = java.awt.Toolkit.getDefaultToolkit().getSystemClipboard().getContents(null);
+		if(contents == null || !contents.isDataFlavorSupported(java.awt.datatransfer.DataFlavor.stringFlavor))
+			return;
+		String text;
+		try {
+			text = (String) contents.getTransferData(java.awt.datatransfer.DataFlavor.stringFlavor);
+		} catch(java.awt.datatransfer.UnsupportedFlavorException e) {
+			((MuisElement) element).msg().error("Badly supported String data flavor", e);
+			return;
+		} catch(java.io.IOException e) {
+			((MuisElement) element).msg().error("I/O exception pasting text", e);
+			return;
+		}
+		element.getDocumentModel().insert(element.getDocumentModel().getCursor(), text);
 	}
 }

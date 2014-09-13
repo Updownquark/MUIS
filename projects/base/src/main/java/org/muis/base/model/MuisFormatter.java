7bc1d7ec436d83248b404e56bad6982d97546e72
Andrew <Updownquark@gmail.com>
2014-09-12 22:25:14 -0600

    Reorganizing TextField around a formatted model value

--- projects/base/src/main/java/org/muis/base/model/MuisFormatter.java
+++ projects/base/src/main/java/org/muis/base/model/MuisFormatter.java
@@ -1,9 +1,17 @@
 package org.muis.base.model;
 
 import org.muis.core.model.MuisDocumentModel;
+import org.muis.core.model.MutableDocumentModel;
 
+/**
+ * Knows how to format and parse objects of a certain type
+ *
+ * @param <T> The type of objects that this formatter can parse and format
+ */
 public interface MuisFormatter<T> {
-	void append(T value, MuisDocumentModel doc);
+	void setText(T value, MutableDocumentModel doc);
+
+	void append(T value, MutableDocumentModel doc);
 
 	T parse(MuisDocumentModel doc);
 }
--- projects/base/src/main/java/org/muis/base/widget/TextField.java
+++ projects/base/src/main/java/org/muis/base/widget/TextField.java
@@ -4,6 +4,9 @@ import static org.muis.base.layout.TextEditLayout.charLengthAtt;
 import static org.muis.base.layout.TextEditLayout.charRowsAtt;
 import static org.muis.core.MuisTextElement.multiLine;
 
+import org.muis.base.model.MuisFormatter;
+import org.muis.core.MuisAttribute;
+import org.muis.core.MuisProperty;
 import org.muis.core.event.FocusEvent;
 import org.muis.core.event.KeyBoardEvent;
 import org.muis.core.model.*;
@@ -16,6 +19,10 @@ import prisms.lang.Type;
 /** A simple widget that takes and displays text input from the user */
 @Template(location = "../../../../text-field.muis")
 public class TextField extends org.muis.core.MuisTemplate implements DocumentedElement {
+	/** Allows specification of the format used by the text field */
+	public static final MuisAttribute<MuisFormatter<?>> format = new MuisAttribute<>("format", new MuisProperty.MuisTypeInstanceProperty<>(
+		(Class<MuisFormatter<?>>) (Class<?>) MuisFormatter.class));
+
 	private org.muis.core.model.WidgetRegistration theRegistration;
 
 	private WrappingDocumentModel theDocumentWrapper;
@@ -27,27 +34,29 @@ public class TextField extends org.muis.core.MuisTemplate implements DocumentedE
 	/** Creates a text field */
 	public TextField() {
 		life().runWhen(() -> {
-			atts().accept(this, charLengthAtt).act(event -> {
+			Object accepter = new Object();
+			atts().accept(accepter, charLengthAtt).act(event -> {
 				try {
 					getElement(getTemplate().getAttachPoint("text")).atts().set(charLengthAtt, event.getValue());
 				} catch(org.muis.core.MuisException e) {
 					msg().error("Could not pass on " + charLengthAtt, e);
 				}
 			});
-			atts().accept(this, charRowsAtt).act(event -> {
+			atts().accept(accepter, charRowsAtt).act(event -> {
 				try {
 					getElement(getTemplate().getAttachPoint("text")).atts().set(charRowsAtt, event.getValue());
 				} catch(org.muis.core.MuisException e) {
 					msg().error("Could not pass on " + charRowsAtt, e);
 				}
 			});
-			atts().accept(this, ModelAttributes.value).act(event -> {
+			atts().accept(accepter, ModelAttributes.value).act(event -> {
 				if(theRegistration != null)
 					theRegistration.unregister();
 				if(event.getValue() instanceof org.muis.core.model.WidgetRegister)
 					theRegistration = ((org.muis.core.model.WidgetRegister) event.getValue()).register(TextField.this);
 			});
-			atts().accept(this, multiLine);
+			atts().accept(accepter, multiLine);
+			atts().accept(accepter, format);
 		}, org.muis.core.MuisConstants.CoreStage.INIT_SELF.toString(), 1);
 		life().runWhen(() -> {
 			initDocument();
@@ -63,23 +72,29 @@ public class TextField extends org.muis.core.MuisTemplate implements DocumentedE
 				msg().error("Could not initialize text layout attributes", e);
 			}
 			ObservableValue.flatten(new Type(Object.class, true), atts().getHolder(ModelAttributes.value)).act(evt -> {
+				if(theCallbackLock > 0)
+					return;
 				modelValueChanged(evt.getOldValue(), evt.getValue());
+			});
+			atts().getHolder(format).act(evt -> {
+				Object mv = atts().get(ModelAttributes.value);
+				modelValueChanged(mv, mv);
 			});
 			getContentModel().addContentListener(evt -> {
 				if(theCallbackLock > 0)
 					return;
+				pushToModel();
 				pushToEdit();
-				fireModelChange();
 			});
 
 			events().filterMap(FocusEvent.blur).act(event -> {
-				pushToModel();
+				pushToContentModel();
 			});
 			events().filterMap(KeyBoardEvent.key.press()).act(event -> {
 				if(event.getKeyCode() == KeyBoardEvent.KeyCode.ENTER) {
 					if(Boolean.TRUE.equals(atts().get(multiLine)) && !event.isControlPressed())
 						return;
-					pushToModel();
+					pushToContentModel();
 				}
 			});
 		}, org.muis.core.MuisConstants.CoreStage.INITIALIZED.toString(), 1);
@@ -139,8 +154,20 @@ public class TextField extends org.muis.core.MuisTemplate implements DocumentedE
 		}
 	}
 
-	/** Pushes content from the edit model to the content model */
 	private void pushToModel() {
+		MutableDocumentModel editModel = getDocumentModel();
+		Object value = parse(editModel);
+		theCallbackLock++;
+		try {
+			fireModelChange(value);
+		} finally {
+			theCallbackLock--;
+		}
+	}
+
+	/** Pushes content from the edit model to the content model */
+	private void pushToContentModel() {
+		pushToModel();
 		MutableDocumentModel contentModel = getContentModel();
 		MutableDocumentModel editModel = getDocumentModel();
 		theCallbackLock++;
@@ -151,42 +178,55 @@ public class TextField extends org.muis.core.MuisTemplate implements DocumentedE
 		} finally {
 			theCallbackLock--;
 		}
-		fireModelChange();
 	}
 
-	private void fireModelChange() {
+	private void fireModelChange(Object value) {
 		ObservableValue<?> modelValue = atts().get(ModelAttributes.value);
-		if(modelValue instanceof org.muis.core.rx.SettableValue && modelValue.getType().canAssignTo(String.class))
-			((org.muis.core.rx.SettableValue<String>) modelValue).set(getContentModel().toString(), null);
+		if(modelValue instanceof org.muis.core.rx.SettableValue)
+			((org.muis.core.rx.SettableValue<Object>) modelValue).set(value, null);
 	}
 
 	private void modelValueChanged(Object oldValue, Object newValue) {
-		if(oldValue instanceof MutableSelectableDocumentModel) {
+		if(oldValue instanceof MutableSelectableDocumentModel && oldValue != newValue) {
 			if(!(newValue instanceof MuisDocumentModel))
 				setContentModel(null);
 		}
 		if(newValue instanceof MuisDocumentModel) {
-			if(newValue instanceof MutableSelectableDocumentModel)
-				setContentModel((MutableSelectableDocumentModel) newValue);
-			else
-				throw new IllegalArgumentException("Document model for a text field must be a "
-					+ MutableSelectableDocumentModel.class.getName());
-			pushToEdit();
+			if(oldValue != newValue) {
+				if(newValue instanceof MutableSelectableDocumentModel)
+					setContentModel((MutableSelectableDocumentModel) newValue);
+				else
+					throw new IllegalArgumentException("Document model for a text field must be a "
+						+ MutableSelectableDocumentModel.class.getName());
+				pushToEdit();
+			}
 		} else if(newValue != null) {
 			if(!theEventLock) {
 				theEventLock = true;
 				try {
-					getContentModel().setText(getTextFor(newValue));
+					setText(getContentModel(), newValue);
 				} finally {
 					theEventLock = false;
 				}
 			}
 			pushToEdit();
 		} else
-			pushToModel();
+			pushToContentModel();
 	}
 
-	private static String getTextFor(Object value) {
-		return value == null ? null : value.toString();
+	private void setText(MutableDocumentModel doc, Object value) {
+		MuisFormatter<?> formatter = atts().get(format);
+		if(formatter != null)
+			((MuisFormatter<Object>) formatter).setText(value, doc);
+		else
+			doc.setText(value == null ? "" : value.toString());
+	}
+
+	private Object parse(MutableDocumentModel doc) {
+		MuisFormatter<?> formatter = atts().get(format);
+		if(formatter != null)
+			return formatter.parse(doc);
+		else
+			return doc.toString();
 	}
 }

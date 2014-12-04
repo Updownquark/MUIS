685cd17b881255d412c16ae73e4e5abe9da18f62
Andrew <Updownquark@gmail.com>
2014-12-04 05:15:47 -0700

    Added a unit test class for styles.  Empty at the moment except for stub methods.

---
+++ projects/MUIS/src/test/java/org/muis/core/style/StylesTest.java
@@ -0,0 +1,17 @@
+package org.muis.core.style;
+
+/** Tests style classes in org.muis.core.style.* packages */
+public class StylesTest {
+	public void testStatefulStyles() {
+
+	}
+
+	public void testStyleSheet() {
+	}
+
+	public void testDependencies() {
+	}
+
+	public void testAllThrough() {
+	}
+}

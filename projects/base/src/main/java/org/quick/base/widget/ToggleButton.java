1ece9436956931612ff1b6219056b24be27df214
Updownquark <Updownquark@gmail.com>
2016-12-05 15:49:58 -0700

    Got the ToggleButtonTest to work to the point where things show up with no errors, but no functionality works

--- projects/base/src/main/java/org/quick/base/widget/ToggleButton.java
+++ projects/base/src/main/java/org/quick/base/widget/ToggleButton.java
@@ -3,6 +3,8 @@ package org.quick.base.widget;
 import org.observe.ObservableAction;
 import org.observe.SettableValue;
 import org.quick.base.BaseConstants;
+import org.quick.core.QuickConstants;
+import org.quick.core.QuickException;
 import org.quick.core.mgr.StateEngine;
 import org.quick.core.model.ModelAttributes;
 import org.quick.core.tags.AcceptAttribute;
@@ -17,7 +19,7 @@ import com.google.common.reflect.TypeToken;
  */
 @QuickElementType(//
 	attributes = { //
-		@AcceptAttribute(declaringClass = ModelAttributes.class, field = "selected")//
+		@AcceptAttribute(declaringClass = ModelAttributes.class, field = "selected", required = true)//
 	}, states = { //
 		@State(name = BaseConstants.States.SELECTED_NAME, priority = BaseConstants.States.SELECTED_PRIORITY)//
 	})
@@ -29,12 +31,25 @@ public class ToggleButton extends Button {
 	/** Creates a toggle button */
 	public ToggleButton() {
 		theSelectedController = state().control(BaseConstants.States.SELECTED);
-		theSelectedController.link(theValue);
+		life().runWhen(() -> {
+			if (atts().getHolder(ModelAttributes.action).get() == null) {
+				try {
+					atts().set(ModelAttributes.action, ObservableAction.and(TypeToken.of(Object.class)));
+				} catch (QuickException e) {
+					msg().error("Could not set default value for action", e);
+				}
+			}
+		}, QuickConstants.CoreStage.STARTUP.toString(), -1);
+		life().runWhen(() -> {
+			theSelectedController.link(theValue);
+		}, QuickConstants.CoreStage.INITIALIZED.toString(), 1);
 	}
 
 	@Override
 	protected ObservableAction<?> createAction() {
 		theValue = atts().getHolder(ModelAttributes.selected).asSettable();
+		theValue = theValue.mapV(theValue.getType(), b -> b == null ? false : b, b -> b,
+			true);
 		return ObservableAction.and(TypeToken.of(Object.class), ObservableAction.flatten(atts().getHolder(ModelAttributes.action)),
 			theValue.assignmentTo(theValue.mapV(v -> !v)));
 	}

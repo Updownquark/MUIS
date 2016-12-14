63603165f53f8f7a34418a507d374a053a3f1579
Updownquark <Updownquark@gmail.com>
2016-12-14 15:46:36 -0700

    Added caches to the DocumentStyleSheet for performance

--- projects/core/src/main/java/org/quick/core/style/DocumentStyleSheet.java
+++ projects/core/src/main/java/org/quick/core/style/DocumentStyleSheet.java
@@ -1,6 +1,12 @@
 package org.quick.core.style;
 
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.observe.collect.ObservableList;
+import org.observe.collect.ObservableSet;
+import org.observe.collect.ObservableSortedSet;
+import org.observe.collect.impl.CachingHashSet;
+import org.observe.collect.impl.CachingTreeSet;
 import org.quick.core.QuickDocument;
 
 import com.google.common.reflect.TypeToken;
@@ -9,6 +15,8 @@ import com.google.common.reflect.TypeToken;
 public class DocumentStyleSheet extends CompoundStyleSheet {
 	private final QuickDocument theDocument;
 	private final ObservableList<StyleSheet> theExternalStyleSheets;
+	private final CachingHashSet<StyleAttribute<?>> theCachedAttributes;
+	private final ConcurrentHashMap<StyleAttribute<?>, CachingTreeSet<? extends StyleConditionValue<?>>> theCachedValues;
 
 	private DocumentStyleSheet(QuickDocument doc, ObservableList<StyleSheet> externalStyleSheets) {
 		super(ObservableList.flattenLists(TypeToken.of(StyleSheet.class), //
@@ -17,6 +25,9 @@ public class DocumentStyleSheet extends CompoundStyleSheet {
 			externalStyleSheets));
 		theDocument=doc;
 		theExternalStyleSheets = externalStyleSheets;
+		// Cache these for performance
+		theCachedAttributes = new CachingHashSet<>(super.attributes());
+		theCachedValues = new ConcurrentHashMap<>();
 	}
 
 	/** @return The document that this style sheet belongs to */
@@ -35,5 +46,16 @@ public class DocumentStyleSheet extends CompoundStyleSheet {
 	 */
 	public static DocumentStyleSheet build(QuickDocument doc) {
 		return new DocumentStyleSheet(doc, ObservableList.constant(TypeToken.of(StyleSheet.class)));
+	}
+
+	@Override
+	public ObservableSet<StyleAttribute<?>> attributes() {
+		return theCachedAttributes;
+	}
+
+	@Override
+	public <T> ObservableSortedSet<StyleConditionValue<T>> getStyleExpressions(StyleAttribute<T> attr) {
+		return (ObservableSortedSet<StyleConditionValue<T>>) theCachedValues.computeIfAbsent(attr,
+			att -> new CachingTreeSet<>(super.getStyleExpressions(attr)));
 	}
 }

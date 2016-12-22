f71eb6a9fadb599f8663fb17951225c3b174d7d3
Andrew <Updownquark@gmail.com>
2016-12-22 08:32:14 -0700

    Just a little tinkering with the ComposedFormatter interface

--- projects/base/src/main/java/org/quick/base/model/ComposedFormatter.java
+++ projects/base/src/main/java/org/quick/base/model/ComposedFormatter.java
@@ -3,14 +3,21 @@ package org.quick.base.model;
 import org.observe.ObservableAction;
 import org.observe.ObservableValue;
 import org.observe.collect.ObservableOrderedCollection;
+import org.quick.core.model.MutableDocumentModel;
 import org.quick.core.model.QuickDocumentModel;
+
+import com.google.common.reflect.TypeToken;
 
 public abstract class ComposedFormatter<T> implements AdjustableFormatter<T> {
 	public interface FormatComponent<T, S> {
-		S getComponent(QuickDocumentModel doc);
+		S getComponent(QuickDocumentModel doc, int[] position);
 		ObservableAction<S> getIncrement();
 		ObservableAction<S> getDecrement();
 	}
+
+	public abstract TypeToken<T> getFormatType();
+
+	public abstract TypeToken<? extends T> getParseType();
 
 	abstract ObservableOrderedCollection<FormatComponent<T, ?>> getComponents(QuickDocumentModel doc);
 
@@ -18,8 +25,35 @@ public abstract class ComposedFormatter<T> implements AdjustableFormatter<T> {
 
 	@Override
 	public QuickFormatter<T> getFormat() {
-		// TODO Auto-generated method stub
-		return null;
+		return new QuickFormatter<T>() {
+			@Override
+			public TypeToken<T> getFormatType() {
+				return ComposedFormatter.this.getFormatType();
+			}
+
+			@Override
+			public void append(MutableDocumentModel doc, T value) {
+				// TODO Auto-generated method stub
+
+			}
+
+			@Override
+			public void adjust(MutableDocumentModel doc, T value) {
+				// TODO Auto-generated method stub
+				QuickFormatter.super.adjust(doc, value);
+			}
+
+			@Override
+			public TypeToken<? extends T> getParseType() {
+				return ComposedFormatter.this.getParseType();
+			}
+
+			@Override
+			public T parse(QuickDocumentModel doc) throws QuickParseException {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		};
 	}
 
 	@Override

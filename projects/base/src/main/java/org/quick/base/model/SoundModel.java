7f66154127b559527760b2fe5e7cd41ccd983439
Updownquark <Updownquark@gmail.com>
2017-01-03 13:36:05 -0700

    Filled out SoundModel most of the way.  A few things missing, but it might be useable now.

--- projects/base/src/main/java/org/quick/base/model/SoundModel.java
+++ projects/base/src/main/java/org/quick/base/model/SoundModel.java
@@ -2,54 +2,207 @@ package org.quick.base.model;
 
 import java.net.URL;
 import java.time.Duration;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicReference;
 
-import org.observe.SimpleObservable;
-import org.observe.SimpleSettableValue;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+import org.observe.*;
+import org.observe.Observable;
+import org.observe.Observer;
 import org.quick.core.QuickParseEnv;
 import org.quick.core.model.*;
 import org.quick.core.parser.QuickParseException;
 import org.quick.core.parser.QuickPropertyParser;
+import org.quick.core.prop.QuickAttribute;
 import org.quick.core.prop.QuickPropertyType;
+
+import com.google.common.reflect.TypeToken;
 
 public class SoundModel implements QuickAppModel {
 	private final SimpleSettableValue<URL> theFile;
 	private final SimpleSettableValue<Duration> theLength;
 
+	private final SimpleSettableValue<Boolean> isPlaying;
 	private final SimpleSettableValue<Double> theRate;
 	private final SimpleSettableValue<Boolean> isLooping;
-	private final SimpleSettableValue<Boolean> isPlaying;
-	/* TODO It would be better if this were just a getter method.  I'd have to spawn an animation just to update this value, which will
-	 * not always be needed and the controls for how often the value is updated should be controlled by a CounterModel.
-	 * Right now QuickAppModel returning a function is not supported.  I could just return an observable value that never fires events. */
-	private final SimpleSettableValue<Duration> thePosition;
-	private final SimpleObservable<Void> theEnd;
+	private final SettableValue<Duration> thePosition;
+	private final SimpleObservable<Object> theEnd;
+
+	private final SettableValue<Double> theExposedRate;
 
 	private final Map<String, Object> theFields;
+
+	private final AtomicReference<Clip> theClip;
 
 	public SoundModel() {
 		theFile = new SimpleSettableValue<>(URL.class, true);
 		theLength = new SimpleSettableValue<>(Duration.class, false);
 		theLength.set(Duration.ZERO, null);
+		theClip = new AtomicReference<>();
 
 		theRate = new SimpleSettableValue<>(double.class, false);
 		theRate.set(1.0, null);
 		isLooping = new SimpleSettableValue<>(boolean.class, false);
 		isLooping.set(false, null);
-		isPlaying = new SimpleSettableValue<>(boolean.class, false);
+		isPlaying = new SimpleSettableValue<Boolean>(boolean.class, false) {
+			@Override
+			public String isAcceptable(Boolean value) {
+				String msg = super.isAcceptable(value);
+				if (msg != null)
+					return msg;
+				if (!value) {
+					Clip clip = theClip.get();
+					if (clip == null)
+						return "No file set";
+				}
+				return null;
+			}
+
+			@Override
+			public ObservableValue<String> isEnabled() {
+				return theFile.mapV(file -> {
+					if (file == null)
+						return "No file set";
+					else if (theClip.get() == null)
+						return "Could not load audio file " + file;
+					return null;
+				});
+			}
+		};
+
+		theExposedRate = theRate.filterAccept(value -> value == null ? "Rate must be non-zero" : null);
+
 		isPlaying.set(false, null);
-		thePosition = new SimpleSettableValue<>(Duration.class, false);
-		thePosition.set(Duration.ZERO, null);
 		theEnd = new SimpleObservable<>();
+
+		theFile.act(evt -> {
+			isPlaying.set(false, evt);
+			loadFile(evt.getValue(), evt);
+		});
+		isPlaying.act(evt -> {
+			if (evt.getValue())
+				start();
+			else
+				stop();
+		});
+		// TODO Use the rate
+		SimpleObservable<Object> internalPositionEvent = new SimpleObservable<>();
+		thePosition = new SettableValue<Duration>() {
+			@Override
+			public TypeToken<Duration> getType() {
+				return TypeToken.of(Duration.class);
+			}
+
+			@Override
+			public boolean isSafe() {
+				return true;
+			}
+
+			@Override
+			public Duration get() {
+				return getPositionValue();
+			}
+
+			@Override
+			public Subscription subscribe(Observer<? super ObservableValueEvent<Duration>> observer) {
+				return Observable.or(theFile.noInit(), theLength.noInit(), isPlaying.noInit(), theEnd, internalPositionEvent).act(evt -> {
+					Duration newPos = get();
+					observer.onNext(createChangeEvent(newPos, newPos, evt));
+				});
+			}
+
+			@Override
+			public Duration set(Duration value, Object cause) throws IllegalArgumentException {
+				Clip clip = theClip.get();
+				if (clip == null)
+					throw new IllegalArgumentException("No clip to set position for");
+				Duration len = Duration.of(clip.getMicrosecondLength(), ChronoUnit.MICROS);
+				if (value.compareTo(len) >= 0)
+					throw new IllegalArgumentException("Position " + value + " must be less than the length (" + len + ")");
+				Duration oldPos = Duration.of(clip.getMicrosecondPosition(), ChronoUnit.MICROS);
+				clip.setMicrosecondPosition(value.toNanos() / 1000);
+				internalPositionEvent.onNext(cause);
+				return oldPos;
+			}
+
+			@Override
+			public String isAcceptable(Duration value) {
+				Clip clip = theClip.get();
+				if (clip == null)
+					return "No clip to set position for";
+				Duration len = Duration.of(clip.getMicrosecondLength(), ChronoUnit.MICROS);
+				if (value.compareTo(len) >= 0)
+					return "Position " + value + " must be less than the length (" + len + ")";
+				return null;
+			}
+
+			@Override
+			public ObservableValue<String> isEnabled() {
+				return isPlaying.isEnabled();
+			}
+		};
 
 		Map<String, Object> fields = new LinkedHashMap<>();
 		fields.put("file", theFile);
+		fields.put("length", theLength.unsettable());
 		fields.put("playing", isPlaying);
-		fields.put("rate", theRate);
+		fields.put("rate", theExposedRate);
+		fields.put("loop", isLooping);
 		fields.put("position", thePosition);
-		fields.put("end", theEnd);
+		fields.put("end", theEnd.readOnly());
 
 		theFields = Collections.unmodifiableMap(fields);
+	}
+
+	public SettableValue<URL> getFile() {
+		return theFile;
+	}
+
+	public SettableValue<Double> getRate() {
+		return theExposedRate;
+	}
+
+	public SettableValue<Boolean> isLooping() {
+		return isLooping;
+	}
+
+	public Duration getPositionValue() {
+		Clip clip = theClip.get();
+		return Duration.of(clip.getMicrosecondPosition(), ChronoUnit.MICROS);
+	}
+
+	private void loadFile(URL file, Object cause) {
+		Clip newClip;
+		try {
+			newClip = AudioSystem.getClip();
+			newClip.open(AudioSystem.getAudioInputStream(file));
+		} catch (Exception e) {
+			throw new IllegalStateException("Could not load audio file " + file, e);
+		}
+		Clip old = theClip.getAndSet(newClip);
+		if (old != null)
+			old.close();
+
+		theLength.set(Duration.of(newClip.getMicrosecondLength(), ChronoUnit.MICROS), cause);
+		//TODO add a listener to be notified when the clip reaches the end
+		// newClip.addLineListener(lineEvent->{
+		// lineEvent.
+		// });
+	}
+
+	private void start() {
+		Clip clip = theClip.get();
+		if (clip != null)
+			clip.start();
+	}
+
+	private void stop() {
+		Clip clip = theClip.get();
+		if (clip != null)
+			clip.stop();
 	}
 
 	@Override
@@ -67,16 +220,12 @@ public class SoundModel implements QuickAppModel {
 		private static QuickModelConfigValidator VALIDATOR;
 		static {
 			VALIDATOR = QuickModelConfigValidator.build()//
-				.forConfig("min", b -> {
-					b.withText(true).atMost(1);
-				}).forConfig("max", b -> {
+				.forConfig("file", b -> {
 					b.withText(true).atMost(1);
 				}).forConfig("rate", b -> {
+					b.withText(true).atMost(1);
+				}).forConfig("loop", b -> {
 					b.withText(true).required();
-				}).forConfig("max-frequency", b -> {
-					b.withText(true).atMost(1);
-				}).forConfig("start", b -> {
-					b.withText(true).atMost(1);
 				}).build();
 		}
 
@@ -84,30 +233,20 @@ public class SoundModel implements QuickAppModel {
 		public QuickAppModel buildModel(QuickModelConfig config, QuickPropertyParser parser, QuickParseEnv parseEnv)
 			throws QuickParseException {
 			VALIDATOR.validate(config);
-			int min = Integer.parseInt(config.getString("min", "0"));
-			int max = Integer.parseInt(config.getString("max", "" + Integer.MAX_VALUE));
-			float rate = 1000f
-				/ QuickPropertyType.duration.getSelfParser().parse(parser, parseEnv, config.getString("rate")).get().toMillis();
-			long maxFrequency = QuickPropertyType.duration.getSelfParser()
-				.parse(parser, parseEnv, config.getString("max-frequency", "10mi")).get().toMillis();
-			String startStr = config.getString("start", "true");
-			if (!"true".equals(startStr) && !"false".equals(startStr))
-				throw new QuickParseException("Invalid value for start: " + startStr);
-			boolean start = "true".equals(startStr);
-			if (Float.isNaN(rate))
-				throw new QuickParseException("NaN not accepted for rate");
-			if (Float.isInfinite(rate))
-				throw new QuickParseException("rate must not be infinite");
-			if (rate <= 0)
-				throw new QuickParseException("rate must be positive");
-			if (max <= min)
-				throw new QuickParseException("max must be greater than min");
-			if (maxFrequency < 10)
-				throw new QuickParseException("max-frequency must be at least 10 milliseconds");
-			// CounterModel counter = new CounterModel(min, max, rate, maxFrequency);
-			if (start)
-				counter.isRunning().set(true, null);
-			return counter;
+			SoundModel model = new SoundModel();
+			if (config.getString("file") != null) {
+				QuickAttribute<URL> fileProp = QuickAttribute.build("file", QuickPropertyType.resource).build();
+				model.getFile().link(parser.parseProperty(fileProp, parseEnv, config.getString("file")));
+			}
+			if (config.getString("rate") != null) {
+				QuickAttribute<Double> rateProp = QuickAttribute.build("rate", QuickPropertyType.floating).build();
+				model.getRate().link(parser.parseProperty(rateProp, parseEnv, config.getString("rate")));
+			}
+			if (config.getString("loop") != null) {
+				QuickAttribute<Boolean> loopProp = QuickAttribute.build("loop", QuickPropertyType.boole).build();
+				model.isLooping().link(parser.parseProperty(loopProp, parseEnv, config.getString("loop")));
+			}
+			return model;
 		}
 	}
 }

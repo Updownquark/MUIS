6deabc443a611e117b33c443f1f691e4f224b328
Andrew <Updownquark@gmail.com>
2017-01-02 21:44:30 -0700

    Javadoc

--- projects/base/src/main/java/org/quick/base/model/QuickAppModelExtension.java
+++ projects/base/src/main/java/org/quick/base/model/QuickAppModelExtension.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import org.quick.core.model.QuickAppModel;
 
+/** A model that extends one or more other models and potentially adds its own fields */
 public class QuickAppModelExtension implements QuickAppModel {
 	private final List<QuickAppModel> theParents;
 	private final Map<String, Object> theFields;
@@ -29,10 +30,17 @@ public class QuickAppModelExtension implements QuickAppModel {
 		return value;
 	}
 
+	/**
+	 * Builds a new extended model
+	 * 
+	 * @param parent The first parent for the model
+	 * @return The builder
+	 */
 	public static Builder build(QuickAppModel parent) {
 		return new Builder().withParent(parent);
 	}
 
+	/** Builds a {@link QuickAppModelExtension} */
 	public static class Builder {
 		private final List<QuickAppModel> theParents;
 		private final Map<String, Object> theFields;
@@ -42,16 +50,26 @@ public class QuickAppModelExtension implements QuickAppModel {
 			theFields = new LinkedHashMap<>();
 		}
 
+		/**
+		 * @param parent Another parent for the model to inherit fields from
+		 * @return This builder
+		 */
 		public Builder withParent(QuickAppModel parent) {
 			theParents.add(parent);
 			return this;
 		}
 
+		/**
+		 * @param name The name of the field
+		 * @param value The value for the field
+		 * @return This builder
+		 */
 		public Builder withField(String name, Object value) {
 			theFields.put(name, value);
 			return this;
 		}
 
+		/** @return The built model */
 		public QuickAppModelExtension build() {
 			return new QuickAppModelExtension(theParents, theFields);
 		}
